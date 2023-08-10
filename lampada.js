6c66cb7f97c3c1e8a5b8253036d8510129d2ce4e
jessicabraga18 <jessicabraga1810@gmail.com>
2023-08-10 09:33:21 -0300

    Inclusão da foto lampada quebrada e alteração do botaoOnOff com o if e else

--- lampada.js
+++ lampada.js
@@ -1,15 +1,36 @@
 let turnOnOff = document.querySelector('#turnOnOff');
 let lamp = document.querySelector('#lamp');
 
+function isLampBroken(){
+    return lamp.src.indexOf('luzQuebrada') > -1
+}
 
 function LampOn(){
-    lamp.src = "img/luzAcesa.jpg"
-    turnOnOff.textContent = "Desligar"
+    if( !isLampBroken()){
+        lamp.src = "img/luzAcesa.jpg"
+    }
 }
 function LampOff(){
-    lamp.src = "img/luzApagada.jpg"
-    turnOnOff.textContent = "Ligar"
-
+    if( !isLampBroken()){
+        lamp.src = "img/luzApagada.jpg"
+    }
+    
 }
-turnOnOff.addEventListener('click',LampOff)
-turnOnOff.addEventListener('dblclick', LampOn)
+function lampBroken(){
+    lamp.src = "img/luzQuebrada.jpg"
+}
+
+function lampOnOff(){
+    if(turnOnOff.textContent == "Ligar"){
+        LampOn();
+        turnOnOff.textContent = "Desligar";
+    } else{
+        LampOff();
+        turnOnOff.textContent = "Ligar";
+    }
+ }
+
+
+turnOnOff.addEventListener('click', lampOnOff)
+lamp.addEventListener('click', lampBroken )
+
